dae8ff29c42db79143a58d253871b56a55254681
EJSeguro <eduardojosesegurodeoliveira@gmail.com>
2026-09-09 23:38:00 -0300

    ATV 6: Criacao das views principais index e show

---
+++ resources/views/alunos/show.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Detalhes do Aluno</title>
+</head>
+<body>
+    <h1>Detalhes do Aluno</h1>
+    <p>Aqui será exibida a ficha completa do aluno selecionado.</p>
+</body>
+</html>
