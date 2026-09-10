0333bfe12cdf4d27455571ea61bcd0e4bde45b4c
EJSeguro <eduardojosesegurodeoliveira@gmail.com>
2026-09-09 23:36:56 -0300

    ATV 2: Rotas com parametro /produto/{id}, /categoria/{id} e /usuario/{id}

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,15 @@ Route::get('/alunos', function () {
 Route::get('/contato', function () {
     return 'Esta é a página de Contato do nosso projeto Laravel.';
 });
+
+Route::get('/produto/{id}', function ($id) {
+    return "Produto ID: {$id}";
+});
+
+Route::get('/categoria/{id}', function ($id) {
+    return "Categoria ID: {$id}";
+});
+
+Route::get('/usuario/{id}', function ($id) {
+    return "Usuario ID: {$id}";
+});
