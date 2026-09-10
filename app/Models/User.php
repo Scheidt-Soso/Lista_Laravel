e19f6b78d00df803693b35352d589d48f7e8a882
Scheidt-Soso <scheidtsalves@gmail.com>
2026-09-09 23:44:33 -0300

    ATV 20: Campo role no User com dois tipos (admin e professor)

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,10 +10,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
+#[Fillable(['name', 'email', 'password', 'role'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
+    public const ROLE_ADMIN = 'admin';
+    public const ROLE_PROFESSOR = 'professor';
+    public const ROLE_ALUNO = 'aluno';
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
@@ -33,5 +36,15 @@ class User extends Authenticatable
     public function aluno()
     {
         return $this->hasOne(Aluno::class);
+    }
+
+    public function isAdmin(): bool
+    {
+        return $this->role === self::ROLE_ADMIN;
+    }
+
+    public function isProfessor(): bool
+    {
+        return $this->role === self::ROLE_PROFESSOR;
     }
 }
