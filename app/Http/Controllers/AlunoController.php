b58004b0ad1a8ee1fa52aea0b3d958effcac264e
EJSeguro <eduardojosesegurodeoliveira@gmail.com>
2026-09-09 23:37:29 -0300

    ATV 4: Implementacao dos 7 metodos CRUD no AlunoController

--- app/Http/Controllers/AlunoController.php
+++ app/Http/Controllers/AlunoController.php
@@ -6,5 +6,38 @@ use Illuminate\Http\Request;
 
 class AlunoController extends Controller
 {
-    //
+    public function index()
+    {
+        return 'Lista de todos os alunos';
+    }
+
+    public function create()
+    {
+        return 'Formulário para cadastrar um novo aluno';
+    }
+
+    public function store(Request $request)
+    {
+        return 'Aluno cadastrado com sucesso';
+    }
+
+    public function show($id)
+    {
+        return "Exibindo dados do aluno de ID: {$id}";
+    }
+
+    public function edit($id)
+    {
+        return "Formulário de edição do aluno de ID: {$id}";
+    }
+
+    public function update(Request $request, $id)
+    {
+        return "Aluno de ID: {$id} atualizado com sucesso";
+    }
+
+    public function destroy($id)
+    {
+        return "Aluno de ID: {$id} removido com sucesso";
+    }
 }
