94e6fd4d9b5d2c9c113a30c58ba7ff4cb4063674
Scheidt-Soso <scheidtsalves@gmail.com>
2026-09-09 23:38:47 -0300

    ATV 10: Criacao do Model Aluno e sua migration

---
+++ app/Models/Aluno.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Aluno extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'nome',
+        'email',
+        'telefone',
+        'curso',
+        'matricula',
+    ];
+
+    public function scopeDeCurso($query, $curso)
+    {
+        return $query->where('curso', $curso);
+    }
+
+    public function scopeNomeContem($query, $palavra)
+    {
+        return $query->where('nome', 'like', "%{$palavra}%");
+    }
+
+    public function scopeRecentes($query)
+    {
+        return $query->latest('created_at');
+    }
+}
